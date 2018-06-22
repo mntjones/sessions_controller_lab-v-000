8be17c656047e5ca04963ec4e88cdb46a9c73d09
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-22 00:06:57 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name] && !params[:name].empty?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else 
+      redirect_to(controller: 'sessions', action: 'new')
+    end 
+  end 
+
+  def destroy 
+    session.delete :name
+    redirect_to(controller: 'application', action: 'hello')
+  end 
+end
