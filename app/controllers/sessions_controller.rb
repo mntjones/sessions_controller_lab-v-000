18f74014d9deaf0c40a07ec0ec5b593b06fb4ef8
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-22 20:22:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
+<<<<<<< HEAD
 
   def create
     if params[:name] && !params[:name].empty?
@@ -15,4 +16,22 @@ class SessionsController < ApplicationController
     session.delete :name
     redirect_to(controller: 'application', action: 'hello')
   end 
+=======
+  
+  def create
+    if params[:username]
+      session[:name] = params[:username]
+      redirect_to '/'
+    else
+      redirect_to '/new'
+    end
+  end
+  
+  def destroy
+    if session[:name]
+      session.delete :name
+    end
+  end
+  
+>>>>>>> 8cddf54e7c1567005258cec32cf5fefb7ea77b09
 end
